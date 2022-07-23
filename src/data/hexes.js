2464c6c76f81bb2f9a062d643a3515e4961355ed
Thom van Oort <thom.vanoort@experius.nl>
2022-07-23 18:02:11 +0200

    added hexes

---
+++ src/data/hexes.js
@@ -0,0 +1,125 @@
+var curseList =
+    [
+        {
+            'description': 'Item shapes shift and morph like an ephemeral haze. Their form eludes the senses, as if the eyes and ears cannot focus.',
+            'imageUrl': 'https://undermine.wiki.gg/images/d/d9/Chaotic_Offering.png',
+            'effect': 'All items are hidden',
+            'rarity': 'Common',
+            'id': '1',
+            'name': 'Chaotic Offering'
+        },
+        {
+            'description': "A mist winds its way through the Undermine. It breathes deadly life into the hollow's inhabitants.",
+            'imageUrl': 'https://undermine.wiki.gg/images/3/37/Dreadful_Fog.png',
+            'effect': 'Enemies have double health and damage',
+            'rarity': 'Common',
+            'id': '2',
+            'name': 'Dreadful Fog'
+        },
+        {
+            'description': 'Hot to the touch, as if they were plucked from the oven.',
+            'imageUrl': 'https://undermine.wiki.gg/images/f/f2/Red-Hot_Nuggets.png',
+            'effect': 'Take damage when picking up gold',
+            'rarity': 'Common',
+            'id': '3',
+            'name': 'Red-Hot Nuggets'
+        },
+        {
+            'description': 'To truly be deemed a rogue, one must rely on their wits, and their wits alone.',
+            'imageUrl': 'https://undermine.wiki.gg/images/e/e6/Rogue%27s_Ultimatum.png',
+            'effect': 'Disables permanent upgrades',
+            'rarity': 'Common',
+            'id': '4',
+            'name': "Rogue's Ultimatum"
+        },
+        {
+            'description': 'While his shield may be purified, the memory of the nightmare lives on.',
+            'imageUrl': 'https://undermine.wiki.gg/images/4/41/Siegfried%27s_Torment.png',
+            'effect': 'Adds an indestructible curse on every floor',
+            'rarity': 'Common',
+            'id': '5',
+            'name': "Siegfried's Torment"
+        },
+        {
+            'description': '"The bite of cold penetrates the body entirely. A curse? No, this is a nightmare." - Unknown',
+            'imageUrl': 'https://undermine.wiki.gg/images/e/ee/Frostbite.png',
+            'effect': 'All healing is reduced to 1',
+            'rarity': 'Common',
+            'id': '6',
+            'name': 'Frostbite'
+        },
+        {
+            'description': 'Preparation, training, and fortitude are not enough. One must be a combination of perfect and lucky to avoid catastrophe.',
+            'imageUrl': 'https://undermine.wiki.gg/images/6/67/Adventurer%27s_Peril.png',
+            'effect': 'Take 17x damage',
+            'rarity': 'Common',
+            'id': '8',
+            'name': "Adventurer's Peril"
+        },
+        {
+            'description': '"What do you do when the glittery trinkets are taken away, and are left to rely on your skill alone?" - Valeen, the Crone',
+            'imageUrl': 'https://undermine.wiki.gg/images/a/a7/Unending_Desolation.png',
+            'effect': 'Floors no longer have relic rooms',
+            'rarity': 'Common',
+            'id': '9',
+            'name': 'Unending Desolation'
+        },
+        {
+            'description': "At each of life's milestones lies the opportunity to wipe the slate clean.",
+            'imageUrl': 'https://undermine.wiki.gg/images/6/610/Amnesia.png',
+            'effect': 'All your relics are destroyed after defeating a boss',
+            'rarity': 'Common',
+            'id': '11',
+            'name': 'Amnesia'
+        },
+        {
+            'description': 'Only the most devout peasants are able to find sustenance in times of famine.',
+            'imageUrl': 'https://undermine.wiki.gg/images/5/5d/Starvation.png',
+            'effect': 'Penance heals, but food and healing potions no longer drop',
+            'rarity': 'Common',
+            'id': '12',
+            'name': 'Starvation'
+        },
+        {
+            'description': "Few can afford the cost of a noble's head, but that of a lowborne? A price many can pay.",
+            'imageUrl': 'https://undermine.wiki.gg/images/e/e3/Nemesis.png',
+            'effect': 'Become prey',
+            'rarity': 'Common',
+            'id': '13',
+            'name': 'Nemesis'
+        },
+        {
+            'description': 'The items have been preserved in thick crystal with the expectations they will be worth something someday.',
+            'imageUrl': 'https://undermine.wiki.gg/images/6/65/Stasis.png',
+            'effect': 'Items are trapped in crystal',
+            'rarity': 'Common',
+            'id': '14',
+            'name': 'Stasis'
+        },
+        {
+            'description': 'We abdicate the throne of self-determination through prayer, whether it is to the light... or the dark.',
+            'imageUrl': 'https://undermine.wiki.gg/images/2/22/Blasphemy.png',
+            'effect': 'Altars are reversed',
+            'rarity': 'Common',
+            'id': '15',
+            'name': 'Blasphemy'
+        },
+        {
+            'description': 'These items are all powerful, but beware, a curse comes with each and every one of them.',
+            'imageUrl': 'https://undermine.wiki.gg/images/4/4e/Accursed.png',
+            'effect': 'All shop items are cursed',
+            'rarity': 'Common',
+            'id': '16',
+            'name': 'Accursed'
+        },
+        {
+            'description': "Haste is not only encouraged, it's required.",
+            'imageUrl': 'https://undermine.wiki.gg/images/c/cb/Terminus.png',
+            'effect': 'Death in 617 minutes, guaranteed',
+            'rarity': 'Common',
+            'id': '18',
+            'name': 'Terminus'
+        },
+    ]
+
+export default hexList;
