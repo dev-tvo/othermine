b36f3d26d5bfd9e1e69b4349c29004b4952d08a2
Thom van Oort <thom.vanoort@experius.nl>
2022-08-03 15:33:48 +0200

    added redirect to /relics if on home

--- src/router/index.js
+++ src/router/index.js
@@ -26,5 +26,26 @@ const router = createRouter({
         }
     ]
 })
+/**
+ * TODO: fix this shit @grootbrendan
+ * Redirects to /relics if the url is not on a router path
+ * */
+
+let routes = router.options.routes;
+let routePaths = []
+
+routes.forEach(route => {
+    routePaths.push(route.path);
+})
+
+for (var i = 0; i < routePaths.length; i++) {
+    if (window.location.href.indexOf(routePaths[i]) > -1) {
+        break
+    } else {
+        window.location.href = '/relics';
+        break;
+    }
+}
+
 
 export default router
