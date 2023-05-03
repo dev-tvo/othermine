ec10563a018a532d09b738378c6c929388126644
Thom <t.vanoort@nobears.nl>
2023-05-03 14:22:17 +0200

    added verel analytisc

--- src/main.js
+++ src/main.js
@@ -1,24 +1,26 @@
-import { createApp } from 'vue'
-import { createPinia } from "pinia/dist/pinia";
+import {createApp} from 'vue'
+import {createPinia} from "pinia/dist/pinia";
+import {inject} from '@vercel/analytics';
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+
 const pinia = createPinia()
 const app = createApp(App)
+
+
+inject();
 
 app.use(pinia)
 app.use(router)
 
 app.mixin({
     data() {
-        return {
-        }
+        return {}
     },
-    methods: {
-    },
-    computed: {
-    },
+    methods: {},
+    computed: {},
     mounted() {
     }
 })
